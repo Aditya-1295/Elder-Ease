8b1ff01b9ac17bee3be0a5c5e6793c36c22626dd
Aditya-1295 <adityaguptamessi@gmail.com>
2020-09-06 11:46:56 +0530

    added main home screen !!!

--- app/src/main/java/com/example/elderease/homeScreen.java
+++ app/src/main/java/com/example/elderease/homeScreen.java
@@ -1,14 +1,36 @@
 package com.example.elderease;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
 
 public class homeScreen extends AppCompatActivity {
+
+    CardView medRem;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_home_screen);
+
+        medRem = findViewById(R.id.medRem);
+
+        medRem.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(homeScreen.this,medicineReminder.class);
+                startActivity(intent);
+            }
+        });
+
+
+
+
+
     }
 }

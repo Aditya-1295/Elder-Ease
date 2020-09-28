1b7550519b92263779131c2d3da5b206232ced13
Ananya Jain <ananya433jain@gmail.com>
2020-09-28 15:29:34 +0530

    magnifier activity added

--- app/src/main/java/com/example/elderease/homeScreen.java
+++ app/src/main/java/com/example/elderease/homeScreen.java
@@ -1,22 +1,33 @@
 package com.example.elderease;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.cardview.widget.CardView;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+
 public class homeScreen extends AppCompatActivity {
 
     CardView medRem;
+    CardView magnify;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_home_screen);
+
+        magnify = findViewById(R.id.magnify);
+
+        magnify.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(homeScreen.this,magnifier.class);
+                startActivity(intent);
+            }
+        });
 
         medRem = findViewById(R.id.medRem);
 
@@ -26,6 +37,7 @@ public class homeScreen extends AppCompatActivity {
                 Intent intent = new Intent(homeScreen.this,medicineReminder.class);
                 startActivity(intent);
             }
+
         });
 
 
---
+++ app/src/main/java/com/example/elderease/showCamera.java
@@ -0,0 +1,73 @@
+package com.example.elderease;
+
+import android.content.Context;
+import android.content.res.Configuration;
+import android.hardware.Camera;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+import androidx.annotation.NonNull;
+
+import java.io.IOException;
+import java.util.List;
+
+public class showCamera extends SurfaceView implements SurfaceHolder.Callback {
+
+    Camera camera;
+    SurfaceHolder holder;
+    public showCamera(Context context, Camera camera) {
+        super(context);
+        this.camera = camera;
+        holder = getHolder();
+        holder.addCallback(this);
+    }
+
+    @Override
+    public void surfaceCreated(@NonNull SurfaceHolder holder) {
+        Camera.Parameters params = camera.getParameters();
+//
+        List<Camera.Size> sizes = params.getSupportedPictureSizes();
+        Camera.Size mSize = null;
+
+        for(Camera.Size size : sizes)
+        {
+            mSize = size;
+        }
+        //change the orientation of the camera
+
+        if(this.getResources().getConfiguration().orientation != Configuration.ORIENTATION_LANDSCAPE)
+        {
+            params.set("orientation", "portrait");
+            camera.setDisplayOrientation(90);
+            params.setRotation(90);
+        }
+        else
+        {
+            params.set("orientation", "landscape");
+            camera.setDisplayOrientation(0);
+            params.setRotation(0);
+        }
+
+        params.setPictureSize(mSize.width, mSize.height);
+
+        camera.setParameters(params);
+        try {
+            camera.setPreviewDisplay(holder);
+            camera.startPreview();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void surfaceChanged(@NonNull SurfaceHolder surfaceHolder, int i, int i1, int i2) {
+
+    }
+
+    @Override
+    public void surfaceDestroyed(@NonNull SurfaceHolder surfaceHolder) {
+        camera.stopPreview();
+        camera.release();
+    }
+}

e78533d13610547eae248fbe0ba33520fc117a85
Aditya-1295 <adityaguptamessi@gmail.com>
2020-11-11 10:47:25 +0530

    SOS configured

---
+++ app/src/main/java/com/example/elderease/DBHelper.java
@@ -0,0 +1,62 @@
+package com.example.elderease;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DBHelper extends SQLiteOpenHelper {
+
+    public static final String DATABASE_NAME = "emergency.db";
+    public static final String TABLE_NAME = "contacts";
+    public static final String PHONE = "phone";
+    public static final String MESSAGE = "message";
+
+
+    public DBHelper(Context context){
+        super(context,DATABASE_NAME,null,1);
+    }
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("create table contacts (id int ,phone1 text,phone2 text,message text)");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int i, int i1) {
+        db.execSQL("DROP TABLE IF EXISTS contacts");
+        onCreate(db);
+    }
+
+    public boolean insertContact (String phone1,String phone2, String message) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("id",1);
+        contentValues.put("phone1", phone1);
+        contentValues.put("phone2", phone2);
+        contentValues.put("message", message);
+        long result = db.insert(TABLE_NAME,null,contentValues);
+        if (result ==-1)
+            return false;
+        else
+            return true;
+
+    }
+
+    public Cursor getData(){
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor cursor = db.rawQuery("select * from contacts where id = 1",null);
+        return cursor;
+    }
+
+    public boolean updateContact(String phone1,String phone2,String message,Integer id){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("phone1",phone1);
+        contentValues.put("phone2",phone2);
+        contentValues.put("message",message);
+        db.update("contacts",contentValues,"id=?",new String[]{String.valueOf(id)});
+        return  true;
+    }
+
+}
--- app/src/main/java/com/example/elderease/sos.java
+++ app/src/main/java/com/example/elderease/sos.java
@@ -1,23 +1,85 @@
 package com.example.elderease;
 
-
-
-
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.telephony.SmsManager;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import java.sql.*;
+
 
 import androidx.appcompat.app.AppCompatActivity;
 
 
 public class sos extends AppCompatActivity {
 
+    EditText phoneOne , phoneTwo, message;
+    Button save;
+    private DBHelper db;
+    int id_To_Update = 0;
+
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sos);
+        this.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
+
+        phoneOne = findViewById(R.id.emergencyNo1);
+        phoneTwo = findViewById(R.id.emergencyNo2);
+        message = findViewById(R.id.message);
+        save = findViewById(R.id.save);
+        db = new DBHelper(this);
+        Cursor cursor = db.getData();
+        if (cursor.getCount() !=0){
+            cursor.moveToFirst();
+            phoneOne.setText(cursor.getString(1));
+            phoneTwo.setText(cursor.getString(2));
+            message.setText(cursor.getString(3));
+        }
 
 
 
+
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+                String numberOne = phoneOne.getText().toString();
+                String numberTwo = phoneTwo.getText().toString();
+                String mess  = message.getText().toString();
+
+                if (cursor.getCount() == 0){
+                    boolean insert = db.insertContact(numberOne,numberTwo,mess);
+
+                    if (insert){
+                        Toast.makeText(getApplicationContext(),"Added Emergency Contact",Toast.LENGTH_SHORT).show();
+                    }
+                }else{
+                    boolean update = db.updateContact(numberOne,numberTwo,mess,1);
+                    if (update){
+                        Toast.makeText(getApplicationContext(),"Updated Emergency Contact",Toast.LENGTH_SHORT).show();
+                        Toast.makeText(getApplicationContext(),"You must restart the app to apply the following changes",Toast.LENGTH_SHORT).show();
+                    }
+
+
+
+                }
+
+                getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
+
+
+            }
+        });
 
     }
+
 }
